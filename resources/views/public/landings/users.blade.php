4c312b418154dd17f072af17fca2ea6abd4abbf2
arsyad z <travisschafer87@yahoo.com>
2022-04-28 07:35:15 +0800

    dept removal

--- resources/views/public/landings/users.blade.php
+++ resources/views/public/landings/users.blade.php
@@ -43,7 +43,7 @@
         class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm text-left" >
             <option value="name" @if (isset($searchby) AND $searchby == 'name') selected @endif>Name</option>
             <option value="phone" @if (isset($searchby) AND $searchby == 'phone') selected @endif>Phone #</option>
-            <option value="dept" @if (isset($searchby) AND $searchby == 'dept') selected @endif>Department</option>
+            {{-- <option value="dept" @if (isset($searchby) AND $searchby == 'dept') selected @endif>Department</option> --}}
         </select>
         <x-jet-button type="submit" class="py-3">Search</x-jet-button>
     </form>
